f2cebd61be337df391c065719dabeb56447b0e2a
Ken Tucker <vb2ae@live.com>
2019-10-04 19:50:52 -0400

    Simple WPF demo

---
+++ CoreWpfDemo/CoreWpfDemo/Services/LaunchDataService.cs
@@ -0,0 +1,31 @@
+﻿using CoreWpfDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWpfDemo.Services
+{
+    public class LaunchDataService : ILaunchDataService
+    {
+        public async Task<ObservableCollection<LaunchData>> GetUpcomingLaunches()
+        {
+
+            ObservableCollection<LaunchData> result = new ObservableCollection<LaunchData>();
+            using (HttpClient http = new HttpClient())
+            {
+                string json = await http.GetStringAsync("http://kentuckerapps.azurewebsites.net/api/spacelaunch");
+                var list = System.Text.Json.JsonSerializer.Deserialize<List<LaunchData>>(json);
+                foreach(var item in list)
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ CoreWpfDemo/CoreWpfDemo/View Models/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CoreWpfDemo.View_Models
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+       internal void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
